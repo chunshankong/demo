87fbdb79b92d208ed3e8d4f096c41b44b47cf290
yangxiaode <980408132@qq.com>
2018-10-13 20:53:12 +0800

    test

---
+++ src/design/observer/step3/Amplifier.java
@@ -0,0 +1,49 @@
+package design.observer.step3;
+
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * 放大器
+ * @author yangsiguo
+ * @date 2018/8/10 16:34
+ */
+public class Amplifier implements ResizeListener {
+
+    JFrame frame;
+    int x, y, width, height;
+
+    public Amplifier() {
+
+        frame = new JFrame();
+        x = 800;
+        y = 0;
+        width = 200;
+        height = 200;
+        //设置窗体的位置及大小
+        frame.setBounds(x, y, width, height);
+//        frame.setUndecorated(true);//去掉窗口的装饰
+        frame.setVisible(true);
+        frame.setAlwaysOnTop(true);//窗体置顶
+        frame.setResizable(false);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                frame.setVisible(false);
+                System.exit(0);
+            }
+        });
+
+    }
+
+    public void mouseClick() {
+        frame.setSize(width += 1, height += 1);
+        frame.repaint();
+    }
+
+    @Override
+    public void resize() {
+        mouseClick();
+    }
+}
---
+++ src/design/observer/step3/ResizeListener.java
@@ -0,0 +1,9 @@
+package design.observer.step3;
+
+/**
+ * @author yangsiguo
+ * @date 2018/8/10 15:41
+ */
+public interface ResizeListener {
+    void resize();
+}
